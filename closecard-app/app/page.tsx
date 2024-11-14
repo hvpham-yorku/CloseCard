4d7a0f1ef4894624b5eff427c715e840848af0f5
itzray116R <rayhaanyaser@gmail.com>
2024-11-14 15:15:48 -0500

    Update app/page.tsx

--- closecard-app/app/page.tsx
+++ closecard-app/app/page.tsx
@@ -1,11 +1,17 @@
-import { currentUser, User } from "@clerk/nextjs/server";
+import { currentUser , User } from "@clerk/nextjs/server";
 
 export default async function Home() {
-  const user = await currentUser();
-  console.log(user);
+  const user = await currentUser ();
+
   return (
     <div className="px -10 p-4">
-      <h1>Welcome <span>{user?.firstName}{user?.lastName}</span></h1>
+      {user ? (
+        <h1>Welcome <span>{user.firstName} {user.lastName}</span></h1>
+      ) : (
+        <h1>
+          Please sign in to access the CloseCard app -------------------------------------------------------------------------------------------------------------------^|^|^|^
+        </h1>
+      )}
     </div>
   );
 }
