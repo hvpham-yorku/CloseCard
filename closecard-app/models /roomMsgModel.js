972754efae07228cd94cee2f1ac770d0ae3fd084
Lawrence P <63280907+LLP17@users.noreply.github.com>
2024-11-21 21:35:11 -0500

    Add rest of the models

--- closecard-app/models /roomMsgModel.js
+++ closecard-app/models /roomMsgModel.js
@@ -1,1 +1,24 @@
 const mongoose = require(`mongoose`); 
+const roomMsgSchema = mongoose.Schema(
+    {
+            room_id : { 
+                type: int, 
+                required: [true, "Please enter a room id"]
+            },   
+             sender_name : { 
+                type: String, 
+                required: [true, "Please enter a name"]
+             },
+             sender_email : { 
+                type : String, 
+                required: [true, "Please enter an email"]
+
+             }, 
+             msg : { 
+                type : String, 
+                required: [true, "Please enter a msg"]
+             }
+    }
+    
+    )
+    const RoomMsg = mongoose.model('RoomMsg', roomMsgSchema); 
