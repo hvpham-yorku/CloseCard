0024434f484861805da25481522f703fef8ac8d7
Lawrence P <63280907+LLP17@users.noreply.github.com>
2024-11-21 22:23:45 -0500

    Add Database connection when join room is connected

--- closecard-app/app.js
+++ closecard-app/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require(`mongoose`);
 const app = express(); 
 const port = 3000; 
-const RoomUser = require('./models/roomUserModel')
+const RoomUser = require('./models/roomUserModel'); 
+const Room = require('./models /roomModel'); 
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: false})); 
@@ -15,10 +16,11 @@ mongoose.connect('mongodb+srv://webserver-user:8Ope4255lLUj1SpS@cluster0.xhn3l.m
 }).catch((error) =>{
 console.log(error)
 });
+
 app.post('/joinroom', async(req,res) =>{ 
    // DATABASE STUFF 
-
    try{ 
+    // REQUEST BODY MUST HAVE ROOM ID, USER_NAME, USER_EMAIL
     const roomuser  = await RoomUser.create(req.body)
     res.status(200).json(RoomUser);
 
@@ -28,13 +30,22 @@ app.post('/joinroom', async(req,res) =>{
    }
     
 }); 
-app.post('/createroom', (req,res) =>{ 
+app.post('/createroom', async(req,res) =>{ 
     // DATABASE STUFF 
-     
+    try{ 
+        // REQUEST BODY MUST HAVE ROOM ID, USER_NAME, USER_EMAIL
+        const room  = await Room.create(req.body)
+        res.status(200).json(Room);
+    
+       } catch (error){ 
+        console.log(error); 
+        res.status(500).json({message: error.message}); 
+       }
  }); 
 
  app.post('/sendmessage', (req,res) =>{ 
     // DATABASE STUFF 
+    
      
  }); 
  app.post('/sendresponse', (req,res) =>{ 
